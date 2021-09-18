cc668a2fa7b9ebf3b1419244f5767a8f43fd7024
carmenmendez <carmenmendez.github.io>
2021-09-17 22:08:48 -0500

    falsificando llamada al server con promesas

--- js/login.js
+++ js/login.js
@@ -1,17 +1,23 @@
 // Datos Falsos
-function fakeLogin(email, password) {
-    if(email === 'test@me.com' && password === '12345') {
-        return {
-            success: true
-        }
-    }
-    return {
-        success: false
-    }
+function login(email, password) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('ya me estoy ejecutando...')
+            if(email === 'test@me.com' && password === '12345') {
+                const response = {
+                    success: true,
+                    userName: 'test',
+                    email: 'test@me.com',
+                    name: 'John',
+                    lastName: 'Wick',
+                }
+                resolve(response)
+            }
+            resolve({ success: false })
+        }, 500)
+    })
 }
 
-
-const loginFormElement = document.getElementById('login-form')
 function handleSubmit(event) {
     // Detener el submit del formulario
     event.preventDefault()
@@ -19,17 +25,21 @@ function handleSubmit(event) {
     const passwordInput = document.querySelector('input[name="password"]');
     const email = emailInput.value;
     const password = passwordInput.value;
-    const response = fakeLogin(email, password)
-
-    if(response.success) {
-        alert('Yeiiiii Bienvenido')
-    } else {
-        alert('Usuario o password inválido')
-    }
 
     // Paso 1: Validacion (TODO: validar datos)
     // Paso 2: mandar los datos al backend
+    const promiseLogin = login(email, password)
+    // Esperar a que la promesa se resuelva o se rechace
+    promiseLogin.then((respuesta) => {
+        if(respuesta.success){
+            console.log('Bienvenido')
+            window.location.href = '/index.html'
+        } else {
+            alert('usuario y password incorrectos')
+        }
+    })
     
     // Paso 3: Reaccionar a la respuesta del backend
 }
+const loginFormElement = document.getElementById('login-form')
 loginFormElement.addEventListener('submit', handleSubmit)
